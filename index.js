9a4923b8f9341b24a388c893e5a526bb2a87303f
rajsaurabh78 <108000350+rajsaurabh78@users.noreply.github.com>
2022-12-14 15:33:09 +0530

    added this

---
+++ index.js
@@ -0,0 +1,32 @@
+let obj=[
+    {
+        name:"SkinStore Clone",
+        img:"",
+        feat:"",
+        tech:"Html | CSS | JavaScript | Github "
+    },
+    {
+        name:"Wrike Clone",
+        img:"",
+        feat:"",
+        tech:"Html | CSS | JavaScript | Github "
+    },
+    {
+        name:"Get Harvest",
+        img:"",
+        feat:"",
+        tech:"Html | CSS | JavaScript | Github "
+    },
+    {
+        name:"",
+        img:"",
+        feat:"",
+        tech:""
+    },
+    {
+        name:"",
+        img:"",
+        feat:"",
+        tech:""
+    }
+]
