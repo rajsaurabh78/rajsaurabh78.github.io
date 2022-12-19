4ab11aab916b388fe9d9cb0e413b9a459187fc1d
rajsaurabh78 <108000350+rajsaurabh78@users.noreply.github.com>
2022-12-19 12:55:23 +0530

    add

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 let obj=[
     {
         name:"SkinStore Clone",
-        img:"",
+        img:"rajsaurabh78.github.io\img\skin.png",
         feat:"",
         tech:"Html | CSS | JavaScript | Github "
     },
     {
         name:"Wrike Clone",
-        img:"",
+        img:"rajsaurabh78.github.io\img\wrike.png",
         feat:"",
         tech:"Html | CSS | JavaScript | Github "
     },
     {
         name:"Get Harvest",
-        img:"",
+        img:"rajsaurabh78.github.io\img\integ.png",
         feat:"",
         tech:"Html | CSS | JavaScript | Github "
-    },
-    {
-        name:"",
-        img:"",
-        feat:"",
-        tech:""
-    },
-    {
-        name:"",
-        img:"",
-        feat:"",
-        tech:""
     }
+    // {
+    //     name:"",
+    //     img:"",
+    //     feat:"",
+    //     tech:""
+    // },
+    // {
+    //     name:"",
+    //     img:"",
+    //     feat:"",
+    //     tech:""
+    // }
 ]
+
+let display=()=>{
+    obj.forEach(el => {
+        let par=document.querySelector("#par");
+
+        let div=document.createElement("div")
+        let div1=document.createElement("div")
+        let div2=document.createElement("div")
+        let div3=document.createElement("div")
+        let div4=document.createElement("div")
+        let div5=document.createElement("div")
+        let h1=document.createElement("h1")
+        h1.innerText
+        let h2=document.createElement("h2")
+        let h3=document.createElement("h2")
+        let img=document.createElement("img")
+
+    });
+}
