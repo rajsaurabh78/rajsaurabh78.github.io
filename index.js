cbf68114228db5a56c8bd42a323d7b5bc9808f42
Saurabh Kumar <rajsaurabh78@gmail.com>
2022-12-19 15:09:30 +0530

    add

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
+
 let obj=[
     {
         name:"SkinStore Clone",
-        img:"rajsaurabh78.github.io\img\skin.png",
+        URL:"rajsaurabh78.github.io\img\skin.png",
         feat:"",
-        tech:"Html | CSS | JavaScript | Github "
+        tech:"Html | CSS | JavaScript | Github ",
     },
     {
         name:"Wrike Clone",
-        img:"rajsaurabh78.github.io\img\wrike.png",
+        URL:"rajsaurabh78.github.io\img\wrike.png",
         feat:"",
         tech:"Html | CSS | JavaScript | Github "
     },
     {
         name:"Get Harvest",
-        img:"rajsaurabh78.github.io\img\integ.png",
+        URL:"rajsaurabh78.github.io\img\integ.png",
         feat:"",
         tech:"Html | CSS | JavaScript | Github "
     }
@@ -32,20 +33,31 @@ let obj=[
 ]
 
 let display=()=>{
-    obj.forEach(el => {
+    obj.forEach((el)=> {
         let par=document.querySelector("#par");
-
         let div=document.createElement("div")
         let div1=document.createElement("div")
         let div2=document.createElement("div")
         let div3=document.createElement("div")
         let div4=document.createElement("div")
-        let div5=document.createElement("div")
         let h1=document.createElement("h1")
-        h1.innerText
+        h1.innerText=el.name;
         let h2=document.createElement("h2")
+        h2.innerText=el.feat;
         let h3=document.createElement("h2")
+        h3.innerText=el.tech;
         let img=document.createElement("img")
-
+        img.src=el.URL;
+        let btn1=document.createElement("button")
+        btn1.innerText="git"
+        let btn2=document.createElement("button")
+        btn2.innerText="net"
+        div1.append(h1)
+        div2.append(img)
+        div3.append(h2,h3)
+        div4.append(btn1,btn2)
+        div.append(div1,div2,div3,div4)
+        par.append(div)
     });
 }
+display();
