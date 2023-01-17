0abb78af9322dfcf44ae836dc7aa940e81cfabf0
Saurabh Kumar <rajsaurabh78@gmail.com>
2023-01-17 22:39:30 +0530

    add

--- index.js
+++ index.js
@@ -61,3 +61,32 @@ let display=()=>{
     });
 }
 display();
+
+
+let image=[
+    "https://static.thcdn.com/images/xlarge/webp/widgets/121-us/04/0712-STDCRE-38046-SS-MH-Photography-July-22-REMAINING-BATCHING-Shot2-1180x450-021804.png",
+    "https://static.thcdn.com/images/xlarge/webp/widgets/121-us/16/0701-STDCRE-38244-SS-BME-Skinstore-July-4-Assets-Shot_01-1180x450-095516.jpg",
+    "https://static.thcdn.com/images/xlarge/webp/widgets/121-us/38/1180x450-082838.jpg",
+    "https://static.thcdn.com/images/xlarge/webp/widgets/121-us/55/0712_THG0034801_SS_JUL_22_SHOT_03_1180x450-061455.jpg"
+    ]
+
+    let leftbtn=document.querySelector("#lbtn")
+    let rightbtn=document.querySelector("#rbtn")
+    let imagestate=0;
+    let crau=document.querySelector("#crauser")
+
+    rightbtn.addEventListener("click",function(){
+        imagestate++;
+        if(imagestate===image.length){
+            imagestate=0;
+        }
+        crau.src=image[imagestate];
+    })
+
+    leftbtn.addEventListener("click",function(){
+        imagestate--;
+        if(imagestate<0){
+            imagestate=image.length-1;
+        }
+        crau.src=image[imagestate];
+    })
