c86703d0b0e15a8d7e3b6ad4f920185257a70c78
Saurabh Kumar <rajsaurabh78@gmail.com>
2023-02-22 17:23:51 +0530

    add

--- index.js
+++ index.js
@@ -4,27 +4,26 @@ let obj=[
         name:"SkinStore Clone",
         URL:"https://d1gzz21cah5pzn.cloudfront.net/img/websites/s/ski/skinstore.com-desktop@2x.1652819931.jpg",
         feat:" "+"\n"+"Skinstore is an online retailer that sells premium beauty, clinical skincare, and luxury spa product.",
-        tech:" Sign-in | Sign-up | Cart page | Navbar | Footer "+"\n"+"\n"+"Techstack  :-"+"\n"+"↪ HTML | CSS | JavaScript | Github "+"\n"+"\n"+"An individual project developed within 4 days.",
-        deploy:"https://spontaneous-crumble-e389b6.netlify.app/",
+        tech:" Sign-in | Sign-up | Cart page | Navbar | Footer "+"\n"+"\n"+"Techstack  :-"+"\n"+"↪ HTML | CSS | JavaScript | Github "+"\n"+"\n"+"An individual project developed within 4 days."
     },
     {
         name:"Wrike Clone",
         URL:"https://management.org/wp-content/uploads/2022/03/Wrike-homepage-1024x483.png",
         feat:" "+"\n"+"Wrike is an easy-to-use tool for streamlining the internal project management and collaboration processes between team members.",
         tech:" Sign-in | Sign-up | Price page | Wrike Blog | Tour page | Help center page"+"\n"+"\n"+"Techstack  :-"+"\n"+"↪ HTML | CSS | JavaScript | Github "+"\n"+"\n"+"Area of responsibility  :-"+"\n"+"↪ WrikeBlog page"+"\n"+"\n"+"A collaborative project developed within 4 days, with a team of 5 members.",
-        deploy:"https://spontaneous-crumble-e389b6.netlify.app/",
+      
     },
     {
         name:"Get Harvest Clone",
         URL:"https://www.getharvest.com/hs-fs/hubfs/raw_assets/public/harvest-theme/images/homepage/test/spread.png?width=1323&name=spread.png",
         feat:" "+"\n"+"Harvest is a cloud-based time tracking tool designed for businesses of all sizes.",
         tech:" Sign-in | Sign-up | Integrations page | Why harvest page | Features page | Customers page"+"\n"+"\n"+"Techstack  :-"+"\n"+"↪ HTML | CSS | JavaScript | Github "+"\n"+"\n"+"Area of responsibility  :-"+"\n"+"↪ Integrations page"+"\n"+"\n"+"A collaborative project developed within 4 days, with a team of 5 members.",
-        deploy:"https://spontaneous-crumble-e389b6.netlify.app/",
+      
     }
 ]
 
 let display=()=>{
-    obj.forEach((el)=> {
+    obj.forEach((el,i)=> {
         let par=document.querySelector("#demo");
         let div=document.createElement("div")
         let div1=document.createElement("div")
@@ -45,19 +44,36 @@ let display=()=>{
         img.src=el.URL;
         let btn1=document.createElement("button")
         btn1.innerText="GitHub"
+        btn1.onclick=(()=>{
+            githubLin(i);
+        })
         let btn2=document.createElement("button")
         btn2.innerText="Deploy"
+        btn2.onclick=(()=>{
+            deployLin(i);
+        })
         btn2.style.textAnchor=el.deploy
         div1.append(h1)
         div2.append(img)
         div3.append(h2,l1,h3,l2)
-        a.append(btn1)
         div4.append(btn1,btn2)
         div.append(div1,div2,div3,div4)
         par.append(div)
     });
 }
 display();
+let deploy=["https://spontaneous-crumble-e389b6.netlify.app/","https://profound-lamington-2fcd8c.netlify.app/index.html","https://vermillion-gumdrop-83c95e.netlify.app/index.html"]
+let github=["https://github.com/rajsaurabh78/precious-birds-1544","https://github.com/Durgesh9871/Wrike","https://github.com/farhaz1o/standing-frog-1055"]
+let deployLin=(i)=>{
+    window.location.href=deploy[i];
+    display()
+}
+let githubLin=(i)=>{
+    window.location.href=github[i];
+    display()
+}
+
+
 
 //+++++++++++++++++++++++++++++++++vavbar
 const mainMenu = document.querySelector('.mainMenu');
